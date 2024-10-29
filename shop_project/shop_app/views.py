789ab104ec40a85c3da235ee61952bb057287880
Alexander Graham <alexinbristol@outlook.com>
2024-10-29 13:15:17 +0000

    Included Django template language.

--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Item
 
 def shop(request):
-    return render(request, 'shop.html')
+    items = Item.objects.all()
+    return render(request, 'shop.html', {'items' : items})
 
 
 # Create your views here.
